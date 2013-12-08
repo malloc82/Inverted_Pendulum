5a50fa00b3d9f9cc6e1830b0ac328562679e3383
Ritchie Cai <ritchiecai@r2labs.org>
2013-12-07 23:55:46 -0600

    motor test update, using forward and backward

--- tests/ev3motor_test.java
+++ tests/ev3motor_test.java
@@ -1,20 +1,32 @@
 import static java.lang.System.out;
+import java.lang.Object;
 
 import lejos.hardware.motor.Motor;
 import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
 
 public class ev3motor_test {
+    Object obj;
     public void motor_test() {
+
         out.println("setting parameters ...");
         RegulatedMotor Motor_r = Motor.A;
         out.println("here");
         Motor_r.resetTachoCount();
         Motor_r.rotateTo(0);
         // Motor_r.setSpeed(200);
-        Motor_r.setAcceleration(200);
         // Motor_r.setAcceleration(800);
         // out.println("start rotating ...");
-        Motor_r.rotateTo(3600);
+        Motor_r.setAcceleration(20);
+        Motor_r.forward();
+        Delay.msDelay(10000);
+
+        out.println("backward rotation");
+
+        Motor_r.setAcceleration(20);
+        Motor_r.backward();
+        Delay.msDelay(50000);
+        
         // Motor_r.rotateTo(-3600);
         // int speed = Motor_r.getSpeed();
         // while(speed > 0 && speed != 360) {
