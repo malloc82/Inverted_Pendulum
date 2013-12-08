1b69eb2f507195a7be6fecd83c39e1226bc34e26
Ritchie Cai <ritchiecai@r2labs.org>
2013-12-07 21:21:24 -0600

    added some new pics and updated some part of the docs

--- tests/ev3motor_test.java
+++ tests/ev3motor_test.java
@@ -5,12 +5,23 @@ import lejos.robotics.RegulatedMotor;
 
 public class ev3motor_test {
     public void motor_test() {
+        out.println("setting parameters ...");
         RegulatedMotor Motor_r = Motor.A;
+        out.println("here");
         Motor_r.resetTachoCount();
         Motor_r.rotateTo(0);
-        Motor_r.setSpeed(10);
-        Motor_r.rotateTo(360);
-        out.println("current speed is : " + Motor_r.getSpeed());
+        // Motor_r.setSpeed(200);
+        Motor_r.setAcceleration(200);
+        // Motor_r.setAcceleration(800);
+        // out.println("start rotating ...");
+        Motor_r.rotateTo(3600);
+        // Motor_r.rotateTo(-3600);
+        // int speed = Motor_r.getSpeed();
+        // while(speed > 0 && speed != 360) {
+        //     out.println("current speed is : " + speed);
+        //     speed = Motor_r.getSpeed();
+        // //     speed = Motor_r.getSpeed();
+        // }
     }
     public static void main(String[] args) {
         out.println("motor test start ...");
